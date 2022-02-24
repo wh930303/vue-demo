df91eec8300f446c9a228bc2467eea1abb702d75
wangh-wang <wangh-wang@neusoft.com>
2022-02-24 22:02:37 +0800

    添加配置项

---
+++ vue.config.js
@@ -0,0 +1,27 @@
+/*
+ * @Author: your name
+ * @Date: 2022-02-24 21:14:04
+ * @LastEditTime: 2022-02-24 21:59:20
+ * @LastEditors: your name
+ * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ * @FilePath: /vue-demo/vue.config.js
+ */
+module.exports = {
+  outputDir: process.env.VUE_APP_ID,
+  publicPath: './',
+  devServer: {
+    open: true,
+    // public: '10.123.7.39:8087',
+    port: 8099,
+    disableHostCheck: true,
+    proxy: {
+      '/dev': {
+        target: 'http://localhost:8888/hsa_seed',
+        changeOrigin: true,
+        pathRewrite: {
+          '^/dev': ''
+        }
+      }
+    }
+  }
+}
