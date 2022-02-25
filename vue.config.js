3987970a605b33b99817350ffbc0369663159b04
wangh-wang <wangh-wang@neusoft.com>
2022-02-25 10:45:11 +0800

    修改配置

--- vue.config.js
+++ vue.config.js
@@ -1,8 +1,8 @@
 /*
  * @Author: your name
  * @Date: 2022-02-24 21:14:04
- * @LastEditTime: 2022-02-24 21:59:20
- * @LastEditors: your name
+ * @LastEditTime: 2022-02-25 10:35:29
+ * @LastEditors: Please set LastEditors
  * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  * @FilePath: /vue-demo/vue.config.js
  */
