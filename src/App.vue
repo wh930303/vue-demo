02f3dd0d6a11ec1226aa4dc81b71ae97128ffbbd
wangh-wang <wangh-wang@neusoft.com>
2022-03-07 19:03:21 +0800

    代码更新

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,13 @@
 <!--
  * @Author: your name
  * @Date: 2022-02-25 10:19:18
- * @LastEditTime: 2022-03-01 20:28:51
- * @LastEditors: your name
+ * @LastEditTime: 2022-03-07 18:41:56
+ * @LastEditors: Please set LastEditors
  * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  * @FilePath: /vue-demo/src/App.vue
 -->
 <template>
   <div id="app">
-    <!-- <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div> -->
     <router-view/>
   </div>
 </template>
--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,7 @@
 /*
  * @Author: your name
  * @Date: 2022-02-24 21:14:04
- * @LastEditTime: 2022-03-06 23:29:11
+ * @LastEditTime: 2022-03-07 18:54:31
  * @LastEditors: Please set LastEditors
  * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  * @FilePath: /vue-demo/vue.config.js
@@ -10,10 +10,10 @@ module.exports = {
   outputDir: process.env.VUE_APP_ID,
   publicPath: './',
   devServer: {
-    open: true,
+    open: true, // 自动打开浏览器
     port: 8088,
     disableHostCheck: true,
-    proxy: {
+    proxy: { // 代理 基于http-proxy-middleware
       '/dev': {
         target: 'http://localhost:8888/demo',
         changeOrigin: true,
